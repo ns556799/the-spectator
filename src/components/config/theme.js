931d570b4ff9fb2e6140b022e295e89869a0151e
Niv Suresh <nivsuresh06@gmail.com>
2020-09-27 22:03:56 +0100

    Update theme.js

--- src/components/config/theme.js
+++ src/components/config/theme.js
@@ -1,5 +1,14 @@
+// Colours
+
 export const venetianRed = "#d30d1d";
-export const lightVenetianRed = "rgba(211, 13, 29, 0.05)";
+export const lightVenetianRed = "rgba(211, 13, 29, 0.1)";
+export const lighterVenetianRed = "rgba(211, 13, 29, 0.05)";
+
 export const nero = "#262222";
 export const lightNero = "rgba(38, 34, 34, 0.4)";
-export const lighterNero = "rgba(38, 34, 34, 0.05)";
+export const lighterNero = "rgba(38, 34, 34, 0.1)";
+export const lightestNero = "rgba(38, 34, 34, 0.05)";
+
+// Fonts
+export const goudyFont = "goudy-old-style";
+export const josefinFont = "josefin-sans";
