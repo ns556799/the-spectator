875f229fcc8ea47d7d554c9b2230c92dd09cfcfd
Niv Suresh <nivsuresh06@gmail.com>
2020-09-27 15:24:37 +0100

    Adding theme via themeprovider

---
+++ src/components/config/theme.js
@@ -0,0 +1,5 @@
+export const venetianRed = "#d30d1d";
+export const lightVenetianRed = "rgba(211, 13, 29, 0.05)";
+export const nero = "#262222";
+export const lightNero = "rgba(38, 34, 34, 0.4)";
+export const lighterNero = "rgba(38, 34, 34, 0.05)";
