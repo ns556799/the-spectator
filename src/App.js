564a32ecf9da190e613bc151facf1ed046e92c0f
Niv Suresh <nivsuresh06@gmail.com>
2020-09-27 13:52:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Articles from "./components/Articles";
 
-function App() {
+const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [articlesPerPage, setArticlesPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      setLoading(true);
+      const res = await axios.get(
+        "https://newsapi.org/v2/top-headlines?country=gb&pageSize=100&apiKey=c8735f91d97b4c03b04490be531d6e13"
+      );
+      setArticles(res.data.articles);
+      setLoading(false);
+    };
+
+    fetchArticles();
+  }, []);
+
+  const indexOfLastArticle = currentPage * articlesPerPage;
+  const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
+  const currentArticles = articles.slice(
+    indexOfFirstArticle,
+    indexOfLastArticle
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Articles articles={currentArticles} loading={loading} />
     </div>
   );
-}
+};
 
 export default App;
