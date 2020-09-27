f42ae62f58166c68bde16a6b08e82d31828f14d6
Niv Suresh <nivsuresh06@gmail.com>
2020-09-27 22:03:50 +0100

    Connecting bookmark bar with selected article context

--- src/components/BookmarkBar.js
+++ src/components/BookmarkBar.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
+import { SelectedArticlesContext } from "../contexts/SelectedArticleContext";
+
 const BookmarkWrapper = styled.div`
   max-width: 25%;
   flex-basis: 25%;
   background-color: red;
   margin-left: 16px;
 `;
+
 const BookmarkBar = () => {
+  const selectedArticles = useContext(SelectedArticlesContext)[0];
   return <BookmarkWrapper>Bookmark</BookmarkWrapper>;
 };
 
