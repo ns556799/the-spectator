3b6593e05ad3d1a8e98c9fbbd7b7c82aa0c82927
Niv Suresh <nivsuresh06@gmail.com>
2020-09-28 13:19:25 +0100

    Adding global styles

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,4 +1,12 @@
-// Colours
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+   body {
+       margin: 0
+   } 
+`;
+
+// Colours - naming from color-blindness.com
 
 export const venetianRed = "#d30d1d";
 export const lightVenetianRed = "rgba(211, 13, 29, 0.1)";
@@ -11,6 +19,7 @@ export const lightestNero = "rgba(38, 34, 34, 0.05)";
 export const borderNero = "rgba(38, 34, 34, 0.15)";
 
 export const white = "#fff";
+export const whiteSmoke = "#ebebeb";
 
 // Fonts
 export const goudyFont = "goudy-old-style";
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { ThemeProvider } from "styled-components";
-import * as theme from "./components/config/theme";
+import * as theme from "./config/theme";
 import Providers from "./contexts/Providers";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <theme.GlobalStyle />
       <Providers>
         <App />
       </Providers>
