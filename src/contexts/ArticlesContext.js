b63d4b56e2f185394eefbcf69e8a30bbe2703369
Niv Suresh <nivsuresh06@gmail.com>
2020-09-29 10:53:51 +0100

    Using local data as NewsAPI needs to be upgraded to paid

--- src/contexts/ArticlesContext.js
+++ src/contexts/ArticlesContext.js
@@ -1,9 +1,11 @@
 import React, { createContext, useState, useEffect } from "react";
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
+
 import { data } from "../results";
 
-const API_KEY = process.env.REACT_APP_API;
+//Must hide API as ENV variable - shown here for demonstrations purposes
+const API_KEY = process.env.REACT_APP_API || "c8735f91d97b4c03b04490be531d6e13";
 
 export const ArticlesContext = createContext();
 
@@ -14,14 +16,19 @@ export const ArticlesProvider = (props) => {
   const [articlesPerPage] = useState(9);
 
   useEffect(() => {
-    const fetchArticles = async () => {
-      setLoading(true);
-      // const res = await axios.get(
-      //   `https://newsapi.org/v2/top-headlines?country=gb&pageSize=100&apiKey=c8735f91d97b4c03b04490be531d6e13`
-      // );
+    fetchArticles();
+  }, []);
 
+  const fetchArticles = async () => {
+    setLoading(true);
+
+    try {
+      // const res = await axios.get(
+      //   `https://newsapi.org/v2/top-headlines?country=gb&pageSize=100&apiKey=${API_KEY}`
+      // );
       // let results = res.data.articles;
 
+      // Due to API limitations on Vercel
       const results = data;
 
       // API does not provide unique IDs to articles, UUID will provide this
@@ -31,10 +38,11 @@ export const ArticlesProvider = (props) => {
 
       setArticles(results);
       setLoading(false);
-    };
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    fetchArticles();
-  }, []);
   return (
     <ArticlesContext.Provider
       value={[articles, loading, currentPage, articlesPerPage, setCurrentPage]}
